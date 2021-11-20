6ecc97e2cde87973c29587031409eb684d679cd2
acgotaku <acgotaku311@gmail.com>
2021-11-20 20:50:31 +0900

    show chainId and status

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { formatEther } from '@ethersproject/units';
 import { AbstractConnector } from '@web3-react/abstract-connector';
 import injected from 'connectors/injected';
 import walletConnect from 'connectors/walletconnect';
-import { useTryConnect } from 'hooks/useWeb3';
+import { useTryConnect, useUserWeb3 } from 'hooks/useWeb3';
 import { useBalance } from 'hooks/useBalance';
 import { trimBalance } from 'utils';
 import './App.css';
 
 function App() {
   const balance = useBalance();
+  const { active, chainId } = useUserWeb3();
   const tryConnect = useTryConnect();
   const connect = useCallback(
     (connector: AbstractConnector) => {
@@ -25,6 +26,12 @@ function App() {
   };
   return (
     <div className="App">
+      <div className="status">
+        connected: {active.toString()}
+      </div>
+      <div className="chainId">
+        chainId: {chainId}
+      </div>
       <div className="balance">{trimBalance(formatEther(balance))}</div>
       <div className="connect">
         <button onClick={inject}>Inject MetaMask</button>
